949a6e71e67f8e10c86a291202f3aa09486f91f1
Sususu <iamsummer614@gmail.com>
2025-03-24 10:14:17 +0900

    migration 완료

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    # modles.Model 이라는 클래스를 상속 받아서 사용할 것이다.
+     title = models.CharField(max_length=100)   
+     content = models.TextField()
+     created_at = models.DateTimeField(auto_now_add=True)  
+     updated_at = models.DateTimeField(auto_now=True)   
+
+# class Comment(models.Model):
+#      pass
